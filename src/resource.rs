46818a93e1fabba17bed587bac8eb104cee715de
Jesse Rossetti <jesse3339@yahoo.com>
2025-04-04 16:56:06 -0400

    wip: need to figure out how to handle resources, perhaps resource types?

--- src/resource.rs
+++ src/resource.rs
@@ -2,13 +2,29 @@ use crate::value_type::ValueBounds;
 
 #[derive(Debug, Clone, Default, PartialEq)]
 pub struct ResourceInstance {
-    pub current: f32,
-    pub bounds: ValueBounds,
+    current: f32,
+    bounds: ValueBounds,
 }
 
 impl ResourceInstance {
     pub fn new(current: f32, bounds: ValueBounds) -> Self {
         Self { current, bounds }
     }
+    
+    pub fn apply_effect(effect: ResourceEffect) {
+        
+    }
+    
+    pub fn get_current(&self) -> f32 {
+        self.current
+    }
 
 }
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ResourceEffect {
+    Flat(f32),
+    Percent(f32)
+}
+
