143a77e5582d3955153174ad4180e2c573d21b36
William Carson <wcarson@bardytechnologies.com>
2026-04-07 09:37:08 -0700

    Update avian.rs

--- src/avian.rs
+++ src/avian.rs
@@ -3,10 +3,13 @@
 //! Enabled by the `avian3d` feature. Reads the `"Mass"` attribute and syncs
 //! it to `avian3d::prelude::Mass`.
 
+use bevy::ecs::query::QueryFilter;
+use bevy::prelude::*;
 use avian3d::prelude::Mass;
 
 use crate::attributes::Attributes;
-use crate::derived::AttributeDerived;
+use crate::derived::{AttributeDerived, InitTo};
+use crate::prelude::AttributesMut;
 
 impl AttributeDerived for Mass {
     fn should_update(&self, attrs: &Attributes) -> bool {
@@ -18,4 +21,11 @@ impl AttributeDerived for Mass {
     }
 }
 
+impl InitTo for Mass {
+    fn init_to_attributes<F: QueryFilter>(&self, entity: Entity, attributes: &mut AttributesMut<'_, '_, F>) {
+        attributes.set(entity, "Mass", self.0);
+    }
+}
+
 crate::register_derived!(Mass);
+crate::register_init_to!(Mass);
